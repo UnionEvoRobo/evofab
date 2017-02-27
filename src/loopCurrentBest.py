02b387c38ea4ca838910544cdfaa72461c8ebfa2
John Peterson <sn0risn@gmail.com>
2017-02-26 20:21:08 -0500

    nice little "watcher" implementation for looping best ANN

--- src/loopCurrentBest.py
+++ src/loopCurrentBest.py
@@ -1,29 +1,34 @@
-from gui_ann_runner import GuiAnnRunner
+from comp_gui_ann_runner import CompGuiAnnRunner
 from grid import Grid
 import ann_io
 import sys
 import getopt
-import sys
+import glob
+import time
 
-helptext = 'loopCurrentBest.py -A annfile -W worldfile'
+helptext = 'loopCurrentBest.py -A annfile'
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "A:W:", ["ANNfile=", "Worldfile="])
+    opts, args = getopt.getopt(sys.argv[1:], "A:", ["ANNfile="])
 except getopt.GetoptError:
     print helptext
     sys.exit()
 
-ann_file = None
-world_file = None
-
 for opt, arg in opts:
     if opt in ('-A', '--ANNfile'):
-        ann_file = arg
-    elif opt in ('-W', '--Worldfile'):
-        world_file = arg
+        ann_dir = arg
 
-while(True):
-	n = ann_io.load(ann_file)
-	ideal_grid = Grid(scale=50, path=world_file)
-	runner = GuiAnnRunner(ideal_grid)
-	runner.run(n, iterations=1000)
+while True:
+    for a in range(3):
+        try:
+            ann_files = glob.glob(ann_dir + str(a) + '/*.ann')
+            nums = [x.split('.')[-2] for x in ann_files]
+            nums = [int(x.split('/')[-1]) for x in nums]
+            num = max(nums)
+
+            ann_file = ann_dir + str(a) + '/' + str(num) + '.ann'
+            n = ann_io.load(ann_file)
+            runner = CompGuiAnnRunner(5, 5, 13, 3, True, 4, 100, draw_each=True)
+            runner.run(n, iterations=200)
+        except:
+            pass
